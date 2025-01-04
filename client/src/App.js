0e3e1e7fcc9b6514ea694db2bedc09152250b5a0
Om Lomate <lomateom7@gmail.com>
2025-01-04 13:08:32 +0530

    app.js issue solved

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import Routes instead of Switch
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
 function App() {
   return (
     <Router>
-      <Routes> {/* Use Routes instead of Switch */}
-        <Route path="/login" element={<Login />} /> {/* Use element prop to render components */}
+      <Routes>
+        {/* Redirect from root / to /login */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        {/* Add other routes like dashboard, home here */}
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       </Routes>
     </Router>
   );
